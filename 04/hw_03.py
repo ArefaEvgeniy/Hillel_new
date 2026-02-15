056e34ef73c11f41fa0675bcafad2f498d3cf659
evgeniy <evgeniy@google.com>
2026-02-15 19:50:31 +0200

    Added homework solutions for lesson 04

--- 04/hw_03.py
+++ 04/hw_03.py
@@ -6,3 +6,9 @@
 # [1, 2, 3, 4, 5, 6, 7, 9] == [1, 3, 7]
 # [1, 1, 2, 1] == [1, 2, 2]
 # [6, 3, 7] == [6, 7, 3]
+
+
+import random
+lst = [random.randint(1, 100) for _ in range(random.randint(3, 10))]
+new_lst = [lst[0], lst[2], lst[-2]]
+print(f"{lst} == {new_lst}")
