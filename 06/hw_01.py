45b5be1b19b6838e005f2a0cebdd28e8a87a9c52
evgeniy <evgeniy@google.com>
2026-02-18 16:44:51 +0200

    Added homework solutions for lesson 06

--- 06/hw_01.py
+++ 06/hw_01.py
@@ -10,3 +10,14 @@
 # "a-a" -> a
 # "s-H" -> stuvwxyzABCDEFGH
 # "a-A" -> abcdefghijklmnopqrstuvwxyzA
+
+
+import string
+
+ascii_str = string.ascii_letters
+user_letters = input("Enter a range of letters in the format 'a-z': ")
+
+first_letter_index = ascii_str.index(user_letters[0])
+second_letter_index = ascii_str.index(user_letters[-1])
+print(ascii_str[first_letter_index:(second_letter_index + 1)])
+
