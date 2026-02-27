e8706fd1e39a12bb40ae7b5e5af07cf2aca8b44b
evgeniy <evgeniy@google.com>
2026-02-27 21:53:33 +0200

    Added homework solutions for lesson 08

--- 08/hw_01.py
+++ 08/hw_01.py
@@ -14,3 +14,15 @@
 # assert add_one([0]) == [1], 'Test3'
 # assert add_one([9]) == [1, 0], 'Test4'
 # print("ОК")
+
+
+def add_one(some_list):
+    some_list_str = ''.join([str(elem) for elem in some_list])
+    return [int(elem) for elem in str(int(some_list_str) + 1)]
+
+
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+assert add_one([0]) == [1], 'Test3'
+assert add_one([9]) == [1, 0], 'Test4'
+print("ОК")
--- 08/hw_02.py
+++ 08/hw_02.py
@@ -10,3 +10,19 @@
 # assert is_palindrome('a.') == True, 'Test3'
 # assert is_palindrome('aurora') == False, 'Test4'
 # print("ОК")
+
+
+import string
+
+
+def is_palindrome(text):
+    text = ''.join(char.lower() for char in text if
+                   char not in string.punctuation and char != ' ')
+    return text == text[::-1]
+
+
+assert is_palindrome('A man, a plan, a canal: Panama') is True, 'Test1'
+assert is_palindrome('0P') is False, 'Test2'
+assert is_palindrome('a.') is True, 'Test3'
+assert is_palindrome('aurora') is False, 'Test4'
+print("ОК")
