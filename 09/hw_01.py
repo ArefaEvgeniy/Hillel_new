0b7ced71fa49a097d263ac6ab1b880dd564e98fd
evgeniy <evgeniy@google.com>
2026-03-02 13:26:38 +0200

    Added homework solutions for lesson 09

--- 09/hw_01.py
+++ 09/hw_01.py
@@ -15,3 +15,14 @@
 # assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
 #                      ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
 # print('OK')
+
+
+def popular_words(text, words):
+    word_list = text.lower().split()
+    result = {word: word_list.count(word) for word in words}
+    return result
+
+
+assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''',
+                     ['i', 'was', 'three', 'near']) == {'i': 4, 'was': 3, 'three': 0, 'near': 0}, 'Test1'
+print('OK')
